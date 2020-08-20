64bf4fbe0b0878ff103a3b6ed93f35ac4fb53c69
Catcher Wong <catcher_hwq@outlook.com>
2020-08-20 08:55:49 +0800

    update ut of yaml parser

--- tests/Nacos.Microsoft.Extensions.Configuration.Tests/YamlConfigurationParserTest.cs
+++ tests/Nacos.Microsoft.Extensions.Configuration.Tests/YamlConfigurationParserTest.cs
@@ -8,11 +8,24 @@
         [Fact]
         public void YamlTest()
         {
-            var yaml = @"";
+            var yaml = @"---
+ConnectionStrings: 
+  Default: Server=127.0.0.1;Port=3306;Database=demo;User Id=root;Password=123456; 
+version: 测试version
+AppSettings: 
+  Str: val
+  num: 1
+  arr: 
+  - 1
+  - 2
+  - 3
+  subobj: 
+    a: b";
 
             var data = Nacos.YamlParser.YamlConfigurationStringParser.Instance.Parse(yaml);
 
             Assert.NotNull(data);
+            Assert.Equal(8, data.Count);
         }
     }
 }
