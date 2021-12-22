71a51f7bf96a47a0c73a2289dd5e71d1454a7ff0
catcherwong <catcher_hwq@outlook.com>
2021-12-22 21:55:04 +0800

    chore: version++

--- src/Nacos/V2/Common/Constants.cs
+++ src/Nacos/V2/Common/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public static string CLIENT_VERSION = "Nacos-CSharp-Client:v1.3.0";
+        public static string CLIENT_VERSION = "Nacos-CSharp-Client:v1.3.1";
 
         public const string ClientName = "NacosClient";
 
