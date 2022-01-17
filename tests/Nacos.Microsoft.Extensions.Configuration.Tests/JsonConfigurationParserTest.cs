e63fae29d2114a70c41d8555c3c9d724ed8f7a23
catcherwong <catcher_hwq@outlook.com>
2022-01-17 14:07:59 +0800

    fix: config parser thread safe issus (#176)

--- tests/Nacos.Microsoft.Extensions.Configuration.Tests/JsonConfigurationParserTest.cs
+++ tests/Nacos.Microsoft.Extensions.Configuration.Tests/JsonConfigurationParserTest.cs
@@ -1,4 +1,4 @@
-namespace Nacos.Microsoft.Extensions.Configuration.Tests
+ï»¿namespace Nacos.Microsoft.Extensions.Configuration.Tests
 {
     using System;
     using Xunit;
@@ -9,11 +9,26 @@ namespace Nacos.Microsoft.Extensions.Configuration.Tests
         [Fact]
         public void JsonTest()
         {
-            var json = @"{""ConnectionStrings"":{""Default"":""Server=127.0.0.1;Port=3306;Database=demo;User Id=root;Password=123456; ""},""version"":""²âÊÔversion"",""AppSettings"":{""Str"":""val"",""num"":1,""arr"":[1,2,3],""subobj"":{""a"":""b""}}}";
+            var json = @"{""ConnectionStrings"":{""Default"":""Server=127.0.0.1;Port=3306;Database=demo;User Id=root;Password=123456; ""},""version"":""æµ‹è¯•version"",""AppSettings"":{""Str"":""val"",""num"":1,""arr"":[1,2,3],""subobj"":{""a"":""b""}}}";
 
             var data = DefaultJsonConfigurationStringParser.Instance.Parse(json);
 
             Assert.NotNull(data);
+            Assert.Equal(8, data.Count);
+        }
+
+        [Fact]
+        public void JsonTest_ThreadSafe()
+        {
+            var json = @"{""ConnectionStrings"":{""Default"":""Server=127.0.0.1;Port=3306;Database=demo;User Id=root;Password=123456; ""},""version"":""æµ‹è¯•version"",""AppSettings"":{""Str"":""val"",""num"":1,""arr"":[1,2,3],""subobj"":{""a"":""b""}}}";
+
+            System.Threading.Tasks.Parallel.For(0, 20, x =>
+            {
+                var data = DefaultJsonConfigurationStringParser.Instance.Parse(json);
+
+                Assert.NotNull(data);
+                Assert.Equal(8, data.Count);
+            });
         }
     }
 }
--- tests/Nacos.Microsoft.Extensions.Configuration.Tests/YamlConfigurationParserTest.cs
+++ tests/Nacos.Microsoft.Extensions.Configuration.Tests/YamlConfigurationParserTest.cs
@@ -28,5 +28,31 @@ AppSettings:
             Assert.NotNull(data);
             Assert.Equal(8, data.Count);
         }
+
+        [Fact]
+        public void YamlTest_ThreadSafe_Iss176()
+        {
+            var yaml = @"---
+ConnectionStrings: 
+  Default: Server=127.0.0.1;Port=3306;Database=demo;User Id=root;Password=123456; 
+version: 测试version
+AppSettings: 
+  Str: val
+  num: 1
+  arr: 
+  - 1
+  - 2
+  - 3
+  subobj: 
+    a: b";
+
+            System.Threading.Tasks.Parallel.For(0, 20, x =>
+            {
+                var data = Nacos.YamlParser.YamlConfigurationStringParser.Instance.Parse(yaml);
+
+                Assert.NotNull(data);
+                Assert.Equal(8, data.Count);
+            });
+        }
     }
 }
