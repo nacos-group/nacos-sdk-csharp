31d4465e16987c00829d63ed6f23bbec12e7afd0
catcherwong <catcher_hwq@outlook.com>
2022-07-02 09:05:45 +0800

    update version to 1.3.4

--- src/Nacos/V2/Common/Constants.cs
+++ src/Nacos/V2/Common/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public static string CLIENT_VERSION = "Nacos-CSharp-Client:v1.3.2";
+        public static string CLIENT_VERSION = "Nacos-CSharp-Client:v1.3.4";
 
         public const string ClientName = "NacosClient";
 
