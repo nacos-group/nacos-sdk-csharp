808f1bd758ee78b8f03aa24ff8c858f63848830d
Memoyu <mmy6076@outlook.com>
2023-10-22 09:47:01 +0800

    feat: add some ParamUtils test

--- tests/Nacos.Tests/Config/Utils/ParamUtilsTest.cs
+++ tests/Nacos.Tests/Config/Utils/ParamUtilsTest.cs
@@ -2,6 +2,7 @@
 {
     using Nacos.Config.Utils;
     using Nacos.Exceptions;
+    using System.Collections.Generic;
     using Xunit;
 
     public class ParamUtilsTest
@@ -39,6 +40,38 @@
             dataId = "dataId";
             ex = Assert.Throws<NacosException>(() => ParamUtils.CheckTdg(tenant, dataId, group));
             Assert.Equal("group invalid", ex.Message);
+        }
+
+        [Fact]
+        public void Check_Key_Param_Should_Exception()
+        {
+            var dataIds = new List<string> { "dataId", "data Id" };
+            var group = "*group";
+            var datumId = "datumId/";
+
+            var ex = Assert.Throws<NacosException>(() => ParamUtils.CheckKeyParam(new List<string>(), group));
+            Assert.Equal("dataId invalid", ex.Message);
+
+            ex = Assert.Throws<NacosException>(() => ParamUtils.CheckKeyParam(dataIds, group));
+            Assert.Equal("dataId invalid", ex.Message);
+
+            ex = Assert.Throws<NacosException>(() => ParamUtils.CheckKeyParam(dataIds[1], group));
+            Assert.Equal("dataId invalid", ex.Message);
+
+            ex = Assert.Throws<NacosException>(() => ParamUtils.CheckKeyParam(dataIds[0], group));
+            Assert.Equal("group invalid", ex.Message);
+
+            group = "group";
+            ex = Assert.Throws<NacosException>(() => ParamUtils.CheckKeyParam(dataIds[0], group, datumId));
+            Assert.Equal("datumId invalid", ex.Message);
+        }
+
+        [Fact]
+        public void Check_Key_Param_By_DatumId_Should_Exception()
+        {
+            var datumId = "datumId/";
+            var ex = Assert.Throws<NacosException>(() => ParamUtils.CheckKeyParam("dataId", "group", datumId));
+            Assert.Equal("datumId invalid", ex.Message);
         }
     }
 }
