3a4c34690ab712112a3eeb268bb2043ba56a2035
Memoyu <mmy6076@outlook.com>
2023-10-29 00:33:01 +0800

    feat: add some util tests

---
+++ tests/Nacos.Tests/Utils/IPUtilTest.cs
@@ -0,0 +1,133 @@
+﻿namespace Nacos.Tests.Utils
+{
+    using Nacos.Utils;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class IPUtilTest
+    {
+        [Fact]
+        public void LocalHostIP_Should_Succeed()
+        {
+            var ip = IPUtil.LocalHostIP();
+            Assert.Equal("127.0.0.1", ip);
+
+            Environment.SetEnvironmentVariable("java.net.preferIPv6Addresses", bool.TrueString);
+            ip = IPUtil.LocalHostIP();
+            Assert.Equal("[::1]", ip);
+
+            Environment.SetEnvironmentVariable("java.net.preferIPv6Addresses", bool.FalseString);
+            IPUtil.PREFER_IPV6_ADDRESSES = bool.Parse(Environment.GetEnvironmentVariable("java.net.preferIPv6Addresses"));
+            ip = IPUtil.LocalHostIP();
+            Assert.Equal("127.0.0.1", ip);
+        }
+
+        [Fact]
+        public void IsIPv4_Should_Succeed()
+        {
+            Assert.True(IPUtil.IsIPv4("192.168.1.1"));
+
+            Assert.False(IPUtil.IsIPv4("192.168.1.1.3"));
+
+            Assert.False(IPUtil.IsIPv4("[::1]"));
+        }
+
+        [Fact]
+        public void IsIPv6_Should_Succeed()
+        {
+            Assert.False(IPUtil.IsIPv6("192.168.1.1"));
+
+            Assert.False(IPUtil.IsIPv6("192.168.1.1.3"));
+
+            Assert.True(IPUtil.IsIPv6("[::1]"));
+        }
+
+        [Fact]
+        public void IsIP_Should_Succeed()
+        {
+            Assert.True(IPUtil.IsIP("192.168.1.1"));
+
+            Assert.True(IPUtil.IsIP("[::1]"));
+
+            Assert.False(IPUtil.IsIP("192.168.1.1.3"));
+
+            Assert.False(IPUtil.IsIP("[::1w-3]"));
+        }
+
+        [Fact]
+        public void SplitIPPortStr_Should_Succeed()
+        {
+            var ipv4 = "192.168.1.1:80";
+            var ipv6 = "[A01F::0]:88";
+
+            var ipv4Port = IPUtil.SplitIPPortStr(ipv4);
+            Assert.Equal(2, ipv4Port.Length);
+            Assert.Equal("192.168.1.1", ipv4Port[0]);
+            Assert.Equal("80", ipv4Port[1]);
+
+            var ipv6Port = IPUtil.SplitIPPortStr(ipv6);
+            Assert.Equal(2, ipv6Port.Length);
+            Assert.Equal("[A01F::0]", ipv6Port[0]);
+            Assert.Equal("88", ipv6Port[1]);
+        }
+
+        [Fact]
+        public void ContainsPort_Should_Succeed()
+        {
+            // regular address
+            var ipv4 = "192.168.1.1:80";
+            var ipv6 = "[A01F::0]:88";
+            var ipv4WithPort = IPUtil.ContainsPort(ipv4);
+            Assert.True(ipv4WithPort);
+
+            var ipv6WithPort = IPUtil.ContainsPort(ipv6);
+            Assert.True(ipv6WithPort);
+
+            // with null port
+            ipv4 = "192.168.1.1:";
+            ipv6 = "[A01F::0]:";
+            ipv4WithPort = IPUtil.ContainsPort(ipv4);
+            Assert.False(ipv4WithPort);
+
+            ipv6WithPort = IPUtil.ContainsPort(ipv6);
+            Assert.False(ipv6WithPort);
+
+            // without port
+            ipv4 = "192.168.1.1";
+            ipv6 = "[A01F::0]";
+            ipv4WithPort = IPUtil.ContainsPort(ipv4);
+            Assert.False(ipv4WithPort);
+
+            ipv6WithPort = IPUtil.ContainsPort(ipv6);
+            Assert.False(ipv6WithPort);
+        }
+
+        [Fact]
+        public void CheckIPs_Should_Succeed()
+        {
+            var ips = new List<string>();
+            var illegalIps = IPUtil.CheckIPs(ips.ToArray());
+            Assert.Equal("ok", illegalIps);
+
+            ips = new List<string> { "[::1]", "[::1]:80", "[344]", "[2AF::1]", "192.168.1.a", "192.168.1.0", "192.168.1.0:88" };
+            illegalIps = IPUtil.CheckIPs(ips.ToArray());
+            Assert.Equal("illegal ip: [344],192.168.1.a,192.168.1.0:88", illegalIps);
+
+            ips = new List<string> { "[::1]", "[::1]:80", "[2AF::1]", "192.168.1.0" };
+            illegalIps = IPUtil.CheckIPs(ips.ToArray());
+            Assert.Equal("ok", illegalIps);
+        }
+
+        [Fact]
+        public void CheckOK_Should_Succeed()
+        {
+            Assert.True(IPUtil.CheckOK("ok"));
+
+            Assert.False(IPUtil.CheckOK("ok1"));
+        }
+    }
+}
--- tests/Nacos.Tests/Utils/ObjectUtilTest.cs
+++ tests/Nacos.Tests/Utils/ObjectUtilTest.cs
@@ -1,12 +1,66 @@
 ﻿namespace Nacos.Tests.Utils
 {
     using Nacos.Utils;
+    using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
     using Xunit;
 
     public class ObjectUtilTest
     {
+        [Fact]
+        public void ToTimestamp_Should_Succeed()
+        {
+            var dt = DateTime.Parse("2023-10-28 10:59:59");
+            var timestamp = dt.ToTimestamp();
+            Assert.Equal(1698490799, timestamp);
+        }
+
+        [Fact]
+        public void SafeGetValue_Should_Succeed()
+        {
+            var dic = new Dictionary<string, object>();
+            var value = dic.SafeGetValue("key", "default-value");
+            Assert.Equal("default-value", value);
+
+            dic.Add("key", "set-value");
+            value = dic.SafeGetValue("key", "default-value");
+            Assert.Equal("set-value", value);
+        }
+
+        [Fact]
+        public async Task ReadFileAsync_Should_Succeed()
+        {
+            var writeContent = "written test content";
+            var file = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt"));
+            await WriteAsync(writeContent, file.FullName).ConfigureAwait(false);
+
+            var readContent = await file.ReadFileAsync().ConfigureAwait(false);
+            Assert.Equal(writeContent, readContent);
+
+            File.Delete(file.FullName);
+        }
+
+        private async Task WriteAsync(string content, string path)
+        {
+            try
+            {
+                using FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite);
+                byte[] bytes = Encoding.UTF8.GetBytes(content);
+                fs.SetLength(bytes.Length);
+                await fs.WriteAsync(bytes, 0, bytes.Length).ConfigureAwait(false);
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
         [Fact]
         public void ToObj_Should_Succeed()
         {
