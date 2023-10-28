df8c2be66554c3ea8f0a66c34e762a1de3791919
Memoyu <mmy6076@outlook.com>
2023-10-28 12:10:54 +0800

    feat: add BalancerTest

---
+++ tests/Nacos.Tests/Naming/Core/BalancerTest.cs
@@ -0,0 +1,60 @@
+﻿namespace Nacos.Tests.Naming.Core
+{
+    using Nacos.Naming.Core;
+    using Nacos.Naming.Dtos;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xunit.Sdk;
+
+    public class BalancerTest
+    {
+        private List<Instance> instances = new List<Instance>();
+        private int healthyIndex = 0;
+
+        public BalancerTest(ITestOutputHelper testOutput)
+        {
+            var count = 10;
+            healthyIndex = new Random().Next(count);
+            testOutput.WriteLine(healthyIndex.ToString());
+            for (int i = 1; i < count + 1; i++)
+            {
+                var instance = new Instance
+                {
+                    InstanceId = Guid.NewGuid().ToString("N"),
+                    Ip = "192.168.0." + i,
+                    Port = 1010 + i,
+                    Weight = i,
+                    Healthy = i == healthyIndex + 1,
+                    ClusterName = "DEFAULT",
+                    ServiceName = "DEFAULT-" + i,
+                    Metadata = new Dictionary<string, string>
+                    {
+                        { "aa", "bb" },
+                        { "cc", "dd" }
+                    }
+                };
+                instances.Add(instance);
+                testOutput.WriteLine(instance.ToString());
+            }
+        }
+
+        [Fact]
+        public void GetHostByRandomWeight_Should_Successed()
+        {
+            var instance = Balancer.GetHostByRandomWeight(instances);
+            Assert.NotNull(instance);
+            Assert.Equal(instances[healthyIndex], instance);
+        }
+
+        [Fact]
+        public void GetHostByRandom_Should_Successed()
+        {
+            var instance = Balancer.GetHostByRandom(instances);
+            Assert.NotNull(instance);
+            Assert.Equal(instances[healthyIndex], instance);
+        }
+    }
+}
