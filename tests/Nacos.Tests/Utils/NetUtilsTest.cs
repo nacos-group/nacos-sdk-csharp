04b3552ae534a1de8de5216924da49ee1dde4873
Memoyu <mmy6076@outlook.com>
2023-10-29 11:35:05 +0800

    feat: add some util test

---
+++ tests/Nacos.Tests/Utils/NetUtilsTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Nacos.Tests.Utils
+{
+    using Nacos.Utils;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class NetUtilsTest
+    {
+        [Fact]
+        public void LocalIP_Should_Succeed()
+        {
+            // Waiting for PR code to be merged
+            var localIp = NetUtils.LocalIP();
+        }
+    }
+}
---
+++ tests/Nacos.Tests/Utils/StringUtilTest.cs
@@ -0,0 +1,54 @@
+﻿namespace Nacos.Tests.Utils
+{
+    using Nacos.Utils;
+    using System.Linq;
+    using Xunit;
+
+    public class StringUtilTest
+    {
+        [Fact]
+        public void SplitByString_Should_Succeed()
+        {
+            var splits = "aa@bb@cc-/@dd".SplitByString("@");
+            Assert.Equal(4, splits.Count());
+            Assert.Equal("cc-/", splits[2]);
+
+            splits = "aa@@cc-/@dd".SplitByString("@");
+            Assert.Equal(3, splits.Count());
+            Assert.Equal("dd", splits[2]);
+        }
+
+        [Fact]
+        public void IsNullOrWhiteSpace_IsNotNullOrWhiteSpace_Should_Succeed()
+        {
+            string str = null;
+            Assert.True(str.IsNullOrWhiteSpace());
+
+            str = "";
+            Assert.True(str.IsNullOrWhiteSpace());
+
+            str = "2";
+            Assert.False(str.IsNullOrWhiteSpace());
+
+            str = null;
+            Assert.False(str.IsNotNullOrWhiteSpace());
+
+            str = "";
+            Assert.False(str.IsNotNullOrWhiteSpace());
+
+            str = "2";
+            Assert.True(str.IsNotNullOrWhiteSpace());
+        }
+
+        [Fact]
+        public void ToEncoded_ecode_Should_Succeed()
+        {
+            var content = "ab@cd$ef%gl-j/k";
+            var encoded = content.ToEncoded();
+            Assert.Equal("ab%40cd%24ef%25gl-j%2Fk", encoded);
+
+            var decode = encoded.ToDecode();
+            Assert.Equal(content, decode);
+        }
+    }
+}
