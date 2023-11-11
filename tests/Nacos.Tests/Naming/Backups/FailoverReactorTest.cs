4cc0247a67507b76f7b451cd2b9d0274d0f6bdf3
Memoyu <mmy6076@outlook.com>
2023-11-11 11:47:35 +0800

    fix: delete using

--- tests/Nacos.Tests/Naming/Backups/FailoverReactorTest.cs
+++ tests/Nacos.Tests/Naming/Backups/FailoverReactorTest.cs
@@ -11,7 +11,6 @@
     using System.Text;
     using System.Threading.Tasks;
     using Xunit;
-    using static Nacos.Tests.Naming.Cache.ServiceInfoHolderTests;
 
     public class FailoverReactorTest
     {
