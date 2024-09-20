f2a24b7b5aae84d44a98afa52a7d3b81e005b89e
左祥 <zuoxiang42@gmail.com>
2024-09-20 10:07:30 +0800

    comment fix

--- src/Nacos.Microsoft.Extensions.ServiceDiscovery/NacosServiceEndpointProviderOptions.cs
+++ src/Nacos.Microsoft.Extensions.ServiceDiscovery/NacosServiceEndpointProviderOptions.cs
@@ -1,7 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-#pragma warning disable SA1200
+﻿#pragma warning disable SA1200
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.ServiceDiscovery;
 #pragma warning restore SA1200
